ed8bdac930b34f14e117e0cf0a6f44e1be824213
carolSutter <carolsutter@gmail.com>
2024-09-10 21:10:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisar() {
+    // Seleciona a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value 
+
+    if(campoPesquisa == "") {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar um idioma...</p>"
+        return}
+
+        campoPesquisa = campoPesquisa.toLowerCase()
+   
+    // Inicializa uma string vazia para armazenar os resultados formatados em HTML
+    let resultados = "";
+    let titulo = "";
+    let link = "";
+    let tags = "";
+
+  
+    // Itera sobre cada idioma nos dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        link = dado.link.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        
+        // Se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || link.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+              <h3>
+                <a href="#" target="_blank">${dado.titulo}</a>
+              </h3>
+              <p class="descrição-meta">${dado.descricao}</p>
+              <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>`;
+        }
+    console.log (dado.titulo.includes(campoPesquisa))
+      // Cria uma nova div para representar um resultado de pesquisa
+      
+    }
+  if (!resultados) {
+resultados = "<p>Nada foi encontrado</p>"
+  }
+    // Atualiza o conteúdo da seção com os resultados formatados
+    section.innerHTML = resultados;
+  }
+
+
---
+++ dados.js
@@ -0,0 +1,81 @@
+let dados = 
+[{
+    titulo: "Inglês",
+    descricao: "Hello, world!",
+    link: "https://pt.wikipedia.org/wiki/L%C3%ADngua_inglesa",
+    tags: "americano americana europeu estadunidense"
+},
+{
+    titulo: "Espanhol",
+    descricao: "Hola, mundo!",
+    link: "https://pt.wikipedia.org/wiki/Idioma_espanhol",
+    tags: "hispânico europeu latino latina"
+},
+{
+    titulo: "Mandarim",
+    descricao: "你好，世界！",
+    link: "https://pt.wikipedia.org/wiki/Língua_mandarim",
+    tags: "chinês oriental xingLing"
+},
+{
+    titulo: "Japonês",
+    descricao: "こんにちは、世界！",
+    link: "https://pt.wikipedia.org/wiki/Língua_japonesa",
+    tags: "asiático japônico"
+},
+{
+    titulo: "Alemão",
+    descricao: "Hallo, Welt!",
+    link: "https://pt.wikipedia.org/wiki/Língua_alemã",
+    tags: "europeu germânico"
+},
+{
+    titulo: "Russo",
+    descricao: "Привет, мир!",
+    link: "https://pt.wikipedia.org/wiki/Língua_russa",
+    tags: "europeu eslavo"
+
+},
+{
+    titulo: "Francês",
+    descricao: "Bonjour, le monde!",
+    link: "https://pt.wikipedia.org/wiki/Língua_francesa",
+    tags: "românico francês europa latina"
+  },
+  {
+    titulo: "Italiano",
+    descricao: "Ciao, mondo!",
+    link: "https://pt.wikipedia.org/wiki/Língua_italiana",
+    tags: "românico italiano europa latina"
+  },
+  {
+    titulo: "Português (Brasil)",
+    descricao: "Olá, mundo!",
+    link: "https://pt.wikipedia.org/wiki/Língua_portuguesa",
+    tags: "românico português brasil américa latina"
+  },
+  {
+    titulo: "Árabe",
+    descricao: "مرحبا بالعالم",
+    link: "https://pt.wikipedia.org/wiki/Língua_árabe",
+    tags: "semítico árabe oriente médio"
+  },
+  {
+    titulo: "Hindi",
+    descricao: "नमस्ते दुनिया",
+    link: "https://pt.wikipedia.org/wiki/Língua_hindi",
+    tags: "indo-europeu hindi índia"
+  },
+  {
+    titulo: "Coreano",
+    descricao: "안녕하세요, 세상!",
+    link: "https://pt.wikipedia.org/wiki/Língua_coreana",
+    tags: "coreano ásia oriental"
+  },
+  {
+    titulo: "Turco",
+    descricao: "Merhaba dünya!",
+    link: "https://pt.wikipedia.org/wiki/Língua_turca",
+    tags: "turco turquia ásia"
+  },
+];
